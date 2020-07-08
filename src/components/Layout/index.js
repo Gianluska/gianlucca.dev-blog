1c931660f98221f426e2101c76fc2aa9ef12b4be
Gianlucca Claudino <gianluccaportela@bne-empregos.com.br>
2020-07-08 18:12:32 -0300

    Refatorando o styled component para uma forma mais organizada

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,30 +2,20 @@ import React from "react"
 import PropTypes from "prop-types"
 import Profile from "../Profile"
 
-import style from 'styled-components'
+import * as S from './styled'
+
 import GlobalStyles from '../../styles/global'
-
-const LayoutWrapper = style.section`
-  display: flex;
-`
-
-const LayoutMain = style.main`
-  background: #16202c;
-  min-height: 100vh;
-  padding: 0 3.75rem 0 20rem;
-  width: 100%;
-`
 
 const Layout = ({ children }) => {
 
   return (
-    <LayoutWrapper>
+    <S.LayoutWrapper>
       <GlobalStyles />
       <aside>
         <Profile />
       </aside>
-      <LayoutMain>{children}</LayoutMain>
-    </LayoutWrapper>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
   )
 }
 
