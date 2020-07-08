bb4bfaf00ba3690e909bf90bf5e4d30e1caab61c
Gianlucca Claudino <gianluccaportela@bne-empregos.com.br>
2020-07-08 18:15:51 -0300

    Estilizando componente de avatar

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import * as S from "./styled"
 
 const Avatar = () => {
   const { avatarImage } = useStaticQuery(
@@ -16,7 +17,7 @@ const Avatar = () => {
       }
     `
   )
-  return <Img fixed={avatarImage.childImageSharp.fixed} />
+  return <S.AvatarWrapper fixed={avatarImage.childImageSharp.fixed} />
 }
 
 export default Avatar
