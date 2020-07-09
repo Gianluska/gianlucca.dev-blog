71d531d5c6247352da3eaeae2f601896bbaa2f06
Gianlucca Claudino <gianluccaportela@bne-empregos.com.br>
2020-07-08 21:32:54 -0300

    Adicionando PostItem Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,19 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from '../components/PostItem'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>My blog</h1>
+    <PostItem
+      slug="/about/"
+      category="Misc"
+      date="30 de Julho de 2019"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+    />
   </Layout>
 )
 
