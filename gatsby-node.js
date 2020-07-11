0590e2fdf5877c299731de0ef9c03f55f59037e4
Gianlucca Claudino <gianluccaportela@bne-empregos.com.br>
2020-07-11 03:08:20 -0300

    criando slugs dinamicos

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,20 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { createFilePath } = require("gatsby-source-filesystem")
 
-// You can delete this file if you're not using it
+// To add the slug field to each post
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: "pages",
+    })
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: `/${slug.slice(12)}`,
+    })
+  }
+}
