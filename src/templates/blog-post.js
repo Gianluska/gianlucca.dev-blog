065a33255ddf7b28a0d851317bcdedeef0f868c4
Gianlucca Claudino <gianluccaportela@bne-empregos.com.br>
2020-07-11 05:05:17 -0300

    estilizando posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,28 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+import * as S from "../components/Post/styled"
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
   return (
-    <>
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} · {post.timeToRead} min de leitura
+        </S.PostDate>
+        <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
+        <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </S.MainContent>
+    </Layout>
   )
 }
 
@@ -17,8 +31,11 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
       html
+      timeToRead
     }
   }
 `
