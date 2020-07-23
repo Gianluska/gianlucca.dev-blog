f27ad7969b52b1debee578ccfb7df9138263315d
Gianlucca Claudino <gianluccaportela@bne-empregos.com.br>
2020-07-23 01:38:10 -0300

    Implementando melhorias de SEO

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,6 +37,7 @@ exports.createPages = ({ graphql, actions }) => {
               date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
               description
               title
+              image
             }
             timeToRead
           }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -15,7 +15,7 @@ const BlogPost = ({ data, pageContext }) => {
 
   return (
     <Layout>
-      <SEO title={post.frontmatter.title} description={post.frontmatter.description} />
+      <SEO title={post.frontmatter.title} description={post.frontmatter.description} image={post.frontmatter.image} />
       <S.PostHeader>
         <S.PostDate>
           {post.frontmatter.date} · {post.timeToRead} min de leitura
@@ -42,6 +42,7 @@ export const query = graphql`
         title
         description
         date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+        image
       }
       html
       timeToRead
