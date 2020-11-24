89b4f861bf8938391d4e2a495967c337192b3251
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2020-11-24 01:38:17 -0300

    adicionando / ao path de thumbnail

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -15,7 +15,7 @@ const BlogPost = ({ data, pageContext }) => {
 
   return (
     <Layout>
-      <SEO title={post.frontmatter.title} description={post.frontmatter.description} image={post.frontmatter.thumbnail} />
+      <SEO title={post.frontmatter.title} description={post.frontmatter.description} image={'/' + post.frontmatter.thumbnail} />
       <S.PostHeader>
         <S.PostDate>
           {post.frontmatter.date} · {post.timeToRead} min de leitura
