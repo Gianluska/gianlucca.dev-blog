7714852c3a22a3da308db3e4ccc622528c9406f9
Gianlucca Claudino <gianluccaportela@bne-empregos.com.br>
2020-11-25 14:56:59 -0300

    adicionando meta tag google search console

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta name="google-site-verification" content="rrgqDs6ZSpBWk3wpBlx2gXuBpckou4U4wuyC3l9QHmk" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes} className="dark">
